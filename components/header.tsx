757b61c9b67fb82e5532f9974e2d00a4d24039de
Alexandrbig1 <alexsmagin1@gmail.com>
2024-03-08 12:50:49 -0600

    feat: added logo. fix: updated header styles

--- components/header.tsx
+++ components/header.tsx
@@ -3,79 +3,87 @@
 import React from "react";
 import { motion } from "framer-motion";
 import Link from "next/link";
+import { GiHandTruck } from "react-icons/gi";
+import { FaTruckFast } from "react-icons/fa6";
 
 export default function Header() {
   return (
     <header className="z-[999] relative">
       <motion.div
-        className="fixed top-6 left-0 h-[6rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] sm:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
-        initial={{ y: -100, opacity: 0 }}
-        animate={{ y: 0, opacity: 1 }}
-      ></motion.div>
-      <nav className="flex fixed top-6 left-0 h-12  py-2 sm:top-[1.7rem] sm:h-[initial] sm:py-0">
-        <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
-          <motion.li
-            className="h-3/4 flex items-center justify-center relative"
-            initial={{ y: -100, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-          >
-            <Link
-              className="flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300"
-              href="#home"
+        className="flex items-center justify-between px-6 py-2  fixed top-6 left-1/2 h-6 w-[90%] rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
+        initial={{ y: -100, x: "-50%", opacity: 0 }}
+        animate={{ y: 0, x: "-50%", opacity: 1 }}
+      >
+        <Link className="flex items-center gap-1.5" href="#home">
+          <FaTruckFast style={{ fontSize: "1.6rem" }} />
+          ASAPmoving
+        </Link>
+        <nav className="">
+          <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
+            <motion.li
+              className="h-3/4 flex items-center justify-center relative"
+              initial={{ y: -100, x: "-50%", opacity: 0 }}
+              animate={{ y: 0, x: "-50%", opacity: 1 }}
             >
-              home
-            </Link>
-          </motion.li>
-          <motion.li
-            className="h-3/4 flex items-center justify-center relative"
-            initial={{ y: -100, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-          >
-            <Link
-              className="flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300"
-              href="#about"
+              <Link
+                className="flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300"
+                href="#home"
+              >
+                home
+              </Link>
+            </motion.li>
+            <motion.li
+              className="h-3/4 flex items-center justify-center relative"
+              initial={{ y: -100, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
             >
-              about
-            </Link>
-          </motion.li>
-          <motion.li
-            className="h-3/4 flex items-center justify-center relative"
-            initial={{ y: -100, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-          >
-            <Link
-              className="flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300"
-              href="#services"
+              <Link
+                className="flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300"
+                href="#about"
+              >
+                about
+              </Link>
+            </motion.li>
+            <motion.li
+              className="h-3/4 flex items-center justify-center relative"
+              initial={{ y: -100, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
             >
-              services
-            </Link>
-          </motion.li>
-          <motion.li
-            className="h-3/4 flex items-center justify-center relative"
-            initial={{ y: -100, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-          >
-            <Link
-              className="flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300"
-              href="#reviews"
+              <Link
+                className="flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300"
+                href="#services"
+              >
+                services
+              </Link>
+            </motion.li>
+            <motion.li
+              className="h-3/4 flex items-center justify-center relative"
+              initial={{ y: -100, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
             >
-              reviews
-            </Link>
-          </motion.li>
-          <motion.li
-            className="h-3/4 flex items-center justify-center relative"
-            initial={{ y: -100, opacity: 0 }}
-            animate={{ y: 0, opacity: 1 }}
-          >
-            <Link
-              className="flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300"
-              href="#contact"
+              <Link
+                className="flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300"
+                href="#reviews"
+              >
+                reviews
+              </Link>
+            </motion.li>
+            <motion.li
+              className="h-3/4 flex items-center justify-center relative"
+              initial={{ y: -100, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
             >
-              contact
-            </Link>
-          </motion.li>
-        </ul>
-      </nav>
+              <Link
+                className="flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300"
+                href="#contact"
+              >
+                contact
+              </Link>
+            </motion.li>
+          </ul>
+        </nav>
+        <div>theme</div>
+      </motion.div>
     </header>
   );
 }
