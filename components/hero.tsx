2981d29c062edc241a63dfada2f265ebbec70b95
Alexandrbig1 <alexsmagin1@gmail.com>
2024-03-10 14:41:41 -0500

    fix: updated hero styles

--- components/hero.tsx
+++ components/hero.tsx
@@ -12,9 +12,16 @@ export default function Hero() {
         height: "43.5rem",
       }}
     >
-      <div className="absolute top-1/2 left-[20%] transform -translate-y-1/2 h-[20rem] w-[20rem] flex flex-col gap-2 items-center justify-center bg-white bg-opacity-10 rounded-[1.2rem] ">
-        <h1>ASAP moving company</h1>
-        <p>company description</p>
+      <div className="absolute top-1/2 left-[15%] transform -translate-y-1/2 h-[20rem] w-[34rem] flex flex-col gap-3 items-start justify-center bg-white bg-opacity-10 rounded-[1.2rem] p-4">
+        <h1 className="text-zinc-100 text-2xl font-medium">
+          ASAP moving company
+        </h1>
+        <p className="font-normal text-zinc-100">
+          ASAP Moving: Your top choice for efficient and stress-free moving
+          services in Chicago. Our dedicated team ensures a smooth transition to
+          your new space, providing reliable and personalized service with every
+          move
+        </p>
       </div>
     </div>
   );
