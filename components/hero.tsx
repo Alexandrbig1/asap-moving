5d88ceccd7d195ba368676ef60c4ee067e0aa99f
Alexandrbig1 <alexsmagin1@gmail.com>
2024-03-11 16:36:50 -0500

    fix: updated hero styles, added buttons

--- components/hero.tsx
+++ components/hero.tsx
@@ -3,16 +3,17 @@ import React from "react";
 export default function Hero() {
   return (
     <div
-      className="h-screen w-full relative"
+      className="h-screen w-full relative bg-black bg-opacity-50"
       style={{
-        background: "url('/hero.jpg')",
+        background:
+          "linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url('/hero.jpg')",
         backgroundSize: "cover",
         backgroundRepeat: "no-repeat",
         backgroundPositionY: "top",
         height: "43.5rem",
       }}
     >
-      <div className="absolute top-1/2 left-[15%] transform -translate-y-1/2 h-[20rem] w-[34rem] flex flex-col gap-3 items-start justify-center bg-white bg-opacity-10 rounded-[1.2rem] p-4">
+      <div className="absolute top-1/2 right-[5%] transform -translate-y-1/2 h-[20rem] w-[42rem] flex flex-col gap-3 items-start justify-center bg-white bg-opacity-10 rounded-[1.2rem] p-4">
         <h1 className="text-zinc-100 text-2xl font-medium">
           ASAP moving company
         </h1>
@@ -22,6 +23,14 @@ export default function Hero() {
           your new space, providing reliable and personalized service with every
           move
         </p>
+        <div className="flex items-center gap-3 mt-4">
+          <button className="p-2 bg-gray-100 rounded-lg w-[12rem]">
+            Contact Us
+          </button>
+          <button className="p-2 bg-gray-100 rounded-lg w-[12rem]">
+            Call Us
+          </button>
+        </div>
       </div>
     </div>
   );
