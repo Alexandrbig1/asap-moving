593e1cc96fb088314a8972a8f5f521051fdec0b9
Alexandrbig1 <alexsmagin1@gmail.com>
2024-03-14 16:12:28 -0500

    feat: added footer component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
+import Footer from "@/components/footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
         <div className="bg-[#e0f2fe] absolute top-0 left-1/2 transform -translate-x-1/2 -z-10 h-[50rem] w-[100%] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
--- components/header.tsx
+++ components/header.tsx
@@ -26,9 +26,9 @@ const headerList = [
 
 export default function Header() {
   return (
-    <header className="z-[999] relative">
+    <header className="z-[999] relative  ">
       <motion.div
-        className="flex items-center justify-between px-6 py-2  fixed top-6 left-1/2 h-6 w-[90%] rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h-[3.25rem] dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
+        className="flex items-center justify-between px-6 py-2  fixed top-6 left-1/2 h-6 w-[90%] rounded-lg border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem]  sm:top-6 sm:h-[3.25rem] dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
         initial={{ y: -100, x: "-50%", opacity: 0 }}
         animate={{ y: 0, x: "-50%", opacity: 1 }}
       >
@@ -37,7 +37,7 @@ export default function Header() {
           ASAPmoving
         </Link>
         <nav className="">
-          <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
+          <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500  sm:w-[initial] sm:flex-nowrap sm:gap-5">
             {headerList.map((item) => (
               <motion.li
                 key={item.title}
