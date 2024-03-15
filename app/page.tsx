058f593aadeae885ba93814701ee65e2d69cb1c6
Alexandrbig1 <alexsmagin1@gmail.com>
2024-03-15 11:39:44 -0500

    feat: added about component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import About from "@/components/about";
 import Hero from "@/components/hero";
 
 export default function Home() {
   return (
     <main className="">
       <Hero />
+      <About />
     </main>
   );
 }
---
+++ components/about.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="flex items-center">
+      <div>
+        <span>icon</span>
+        <p>description</p>
+      </div>
+      <div>
+        <span>icon</span>
+        <p>description</p>
+      </div>
+      <div>
+        <span>icon</span>
+        <p>description</p>
+      </div>
+      <div>
+        <span>icon</span>
+        <p>description</p>
+      </div>
+    </div>
+  );
+}
