7675705275681c385a37266325d44b1c4818e908
Alexandrbig1 <alexsmagin1@gmail.com>
2024-03-16 12:18:46 -0500

    fix: updated about component

--- components/about.tsx
+++ components/about.tsx
@@ -2,20 +2,20 @@ import React from "react";
 
 export default function About() {
   return (
-    <div className="flex items-center">
-      <div>
+    <div className="flex items-center justify-center gap-9 mt-9 w-full">
+      <div className="p-4 bg-sky-50 rounded-md">
         <span>icon</span>
         <p>description</p>
       </div>
-      <div>
+      <div className="p-4 bg-sky-50 rounded-md">
         <span>icon</span>
         <p>description</p>
       </div>
-      <div>
+      <div className="p-4 bg-sky-50 rounded-md">
         <span>icon</span>
         <p>description</p>
       </div>
-      <div>
+      <div className="p-4 bg-sky-50 rounded-md">
         <span>icon</span>
         <p>description</p>
       </div>
