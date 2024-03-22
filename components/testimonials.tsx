6d4e997e3b35816cd39838239ef623d26652b908
Alexandrbig1 <alexsmagin1@gmail.com>
2024-03-21 19:13:56 -0500

    fix: updated testimonials section

--- components/testimonials.tsx
+++ components/testimonials.tsx
@@ -1,5 +1,32 @@
 import React from "react";
 
 export default function Testimonials() {
-  return <div>testimonials</div>;
+  return (
+    <div>
+      <div>
+        <h4>Andrew Thompson</h4>
+        <span>Art Director</span>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus,
+          omnis.
+        </p>
+      </div>
+      <div>
+        <h4>Andrew Thompson</h4>
+        <span>Art Director</span>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus,
+          omnis.
+        </p>
+      </div>
+      <div>
+        <h4>Andrew Thompson</h4>
+        <span>Art Director</span>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus,
+          omnis.
+        </p>
+      </div>
+    </div>
+  );
 }
